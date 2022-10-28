71475190ca07158b33a3d4c57e05ca7f11cdac31
Xiao (Leo) Liu <xxl697@case.edu>
2022-10-28 16:07:51 -0700

    minor fix

--- torch_diff_filter/engine.py
+++ torch_diff_filter/engine.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from dataset import KITTIDataset
 from model import Ensemble_KF
 from torch.utils.tensorboard import SummaryWriter
@@ -14,10 +15,12 @@ class Engine():
         self.model = Ensemble_KF(self.num_ensemble, self.dim_x, self.dim_z)
 
     def train(self):
+        loss = nn.MSELoss()
         dataloader = torch.utils.data.DataLoader(self.dataset, batch_size = self.batch_size,
                                           shuffle=True, num_workers=1)
         pytorch_total_params = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
         print('Total number of parameters: ',pytorch_total_params)
+        print(list(self.model.children())[0])
         # for state_gt, state_pre, obs, raw_obs in dataloader:
         #     # define the training curriculum
 
