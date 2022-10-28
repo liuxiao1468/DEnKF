d00d19c3c49fc24e0b54765b90b5911cb4da0339
Xiao (Leo) Liu <xxl697@case.edu>
2022-10-28 15:54:22 -0700

    add torch version

---
+++ torch_diff_filter/dataset/dataloader.py
@@ -0,0 +1,106 @@
+import os
+import random
+from PIL import Image
+import cv2
+import numpy as np
+from torch.utils.data import Dataset
+import pickle
+import torch
+from einops import rearrange, repeat
+
+class transform:
+    def __init__(self):
+        super(transform, self).__init__()
+        parameters = pickle.load(open('./dataset/full_parameters.pkl', 'rb'))
+        self.state_m = parameters['state_m']
+        self.state_std = parameters['state_std']
+        self.obs_m = parameters['obs_m']
+        self.obs_std = parameters['obs_std']
+
+    def state_transform(self, state):
+        '''
+        state -> [num_ensemble, dim_x]
+        '''
+        state = (state - self.state_m)/self.state_std
+        return state
+
+    def obs_transform(self, obs):
+        '''
+        obs -> [num_ensemble, dim_z]
+        '''
+        obs = (obs - self.obs_m)/self.obs_std
+        return obs
+
+    def state_inv_transform(self, state):
+        '''
+        state -> [num_ensemble, dim_x]
+        '''
+        state = (state * self.state_std) + self.state_m 
+        return state
+
+    def obs_inv_transform(self, obs):
+        '''
+        obs -> [num_ensemble, dim_z]
+        '''
+        obs = (obs * self.obs_std) + self.obs_m 
+        return obs
+
+class KITTIDataset(Dataset):
+    # Basic Instantiation
+    def __init__(self):
+        self.dataset_path = '/Users/xiao.lu/project/KITTI_dataset/'
+        self.dataset = pickle.load(open('./dataset/KITTI_VO_dataset.pkl', 'rb'))
+        self.dataset_length = len(self.dataset)
+        self.transform_ = transform()
+
+    def preprocessing(self, data, mode):
+        img_2 = cv2.imread(self.dataset_path+data[3][1])
+        img_1 = cv2.imread(self.dataset_path+data[3][0])
+        # img_2 = cv2.imread(data[3][1])
+        # img_1 = cv2.imread(data[3][0])
+        # if mode == 'train':
+        #     img_2 = cv2.flip(img_2, 1)
+        #     img_1 = cv2.flip(img_1, 1)
+        # 150, 50
+        # 640, 192
+        img_2 = cv2.resize(img_2, (150, 50), interpolation=cv2.INTER_LINEAR)
+        img_1 = cv2.resize(img_1, (150, 50), interpolation=cv2.INTER_LINEAR)
+        img_2_ = img_2.astype(np.float32)/255.
+        img_1_ = img_1.astype(np.float32)/255.
+        diff = img_2_ - img_1_
+        diff = diff*0.5 + 0.5
+        img = np.concatenate((img_2_, diff), axis=-1)
+        return img
+
+    # Length of the Dataset
+    def __len__(self):
+        self.dataset_length = 6
+        return self.dataset_length
+
+    # Fetch an item from the Dataset
+    def __getitem__(self, idx):
+        state_gt = torch.tensor(self.dataset[idx][1], dtype=torch.float32)
+        state_pre = torch.tensor(self.dataset[idx][0], dtype=torch.float32)
+        obs = torch.tensor(self.dataset[idx][2], dtype=torch.float32)
+        raw_obs = torch.tensor(self.preprocessing(self.dataset[idx], 
+            mode='train'), dtype=torch.float32)
+
+        state_gt = rearrange(state_gt, '(k dim) -> k dim', k=1)
+        state_pre = rearrange(state_pre, '(k dim) -> k dim', k=1)
+        obs = rearrange(obs, '(k dim) -> k dim', k=1)
+        raw_obs = rearrange(raw_obs, 'h w c -> c h w')
+
+        # apply the transformation
+        state_gt = self.transform_.state_transform(state_gt)
+        state_pre = self.transform_.state_transform(state_pre)
+        obs = self.transform_.obs_transform(obs)
+
+        return state_gt, state_pre, obs, raw_obs
+
+# if __name__ == '__main__':
+#     dataset = KITTIDataset()
+#     dataloader = torch.utils.data.DataLoader(dataset, batch_size=2,
+#                                           shuffle=True, num_workers=1)
+#     for state_gt, state_pre, obs, raw_obs in dataloader:
+#         print(obs)
+#         # print(raw_obs.shape)
---
+++ torch_diff_filter/engine.py
@@ -0,0 +1,27 @@
+import torch
+from dataset import KITTIDataset
+from model import Ensemble_KF
+from torch.utils.tensorboard import SummaryWriter
+
+class Engine():
+    def __init__(self):
+        self.batch_size = 2
+        self.dim_x = 5
+        self.dim_z = 2
+        self.num_ensemble = 32
+
+        self.dataset = KITTIDataset()
+        self.model = Ensemble_KF(self.num_ensemble, self.dim_x, self.dim_z)
+
+    def train(self):
+        dataloader = torch.utils.data.DataLoader(self.dataset, batch_size = self.batch_size,
+                                          shuffle=True, num_workers=1)
+        pytorch_total_params = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
+        print('Total number of parameters: ',pytorch_total_params)
+        # for state_gt, state_pre, obs, raw_obs in dataloader:
+        #     # define the training curriculum
+
+if __name__ == '__main__':
+    train_engine = Engine()
+    train_engine.train()
+
